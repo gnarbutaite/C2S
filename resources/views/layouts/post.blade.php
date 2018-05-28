7abe803d419c3c72746e94ee787a7ce7021d1a71
fictionalfridge <jesse_vervaart@hotmail.com>
2018-05-28 09:57:25 +0200

    fixed gintares bullshit

--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -12,11 +12,11 @@
             <div class="[ col-xs-12 col-sm-offset-x1 justify-content-lg-center ]">
                 <div class="[ panel panel-default ] panel-google-plus">
 
-                    <div class=".card-header" style="padding-left:40px">
+                    <div class="card-header" style="padding-left:40px">
                         <h1><b>{{$ad->title}}</b></h1>
                     </div>
 
-                    <div class=".card-block">
+                    <div class="card-block">
                         <img style="width: 30%; height: 30%" class="card-img-top img-fluid img-circle col-4 mx-auto d-flex justify-content-center flex-wrap" src="/uploads/avatars/{{$ad->user->avatar}}" alt="Card image cap" />
                         <h3 class="text-center" ><b>{{$ad->user->name}}</b></h3>
                     </div>
@@ -29,8 +29,15 @@
 
                         <p class="card-text col-lg-12 text-center"><strong>{{$ad->description}}</strong></p>
 
-                    <div class="..card-footer">
-                        <button type="button" class="btn btn-lg btn-info btn-block">Contact the coach</button>
+                    <div class="card-footer">
+                        <form action="/new-chat" method="post">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="coach_id" value="{{$ad->user->id}}">
+                            <input type="hidden" name="ad_id" value="{{$ad->id}}">
+
+                            <input type="submit">
+                        </form>
+                        <a href="http://www.pornhub.com/" class="btn btn-lg btn-info btn-block">Contact the coach</a>
                     </div>
                 </div>
             </div>
